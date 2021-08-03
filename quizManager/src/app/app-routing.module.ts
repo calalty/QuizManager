d7c52b3261ffd06183abc2602f28e9008cc54f0c
Code Nation Student <codenationstudent@Codes-MacBook-Pro.local>
2021-08-03 17:24:52 +0100

    added default route

--- quizManager/src/app/app-routing.module.ts
+++ quizManager/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'dashboard/:id', component: DashboardComponent},
